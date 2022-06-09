2590d3656353dd4912eda4e11f56c30996bf4469
user <kgalsjuwtjuatsnh@asd.edu>
2022-06-09 15:25:31 +0300

    ADD: Now can run main.py to test it (for those not using PyCharm)
    NEXT: test db call
    
    TODO: check docs if dependencies.py is where middleware goes. https://fastapi.tiangolo.com/tutorial/bigger-applications/#another-module-with-apirouter

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+import uvicorn
 from fastapi import FastAPI
 
 from routers import get_all_movies
 
-
 app = FastAPI()
-
 app.include_router(get_all_movies.router)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+    # Visit http://127.0.0.1:8000/docs# to test the endpoints.
