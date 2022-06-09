3e7ebe8d7a8406e9f2bffb9643e3c9a70407108b
user <kgalsjuwtjuatsnh@asd.edu>
2022-06-09 18:35:16 +0300

    ADD: config.py ADD: fallback to config when env-variable not set. CHG: removed raise from secret_handler.py
    TODO: check docs if dependencies.py is where middleware goes. https://fastapi.tiangolo.com/tutorial/bigger-applications/#another-module-with-apirouter

--- secret_handler.py
+++ secret_handler.py
@@ -1,13 +1,23 @@
 import os
 
-
 PASS_ENV_VAR = 'MONGODB_MOVIES_USER0_PASS'
+
+
+def fallback_db_password():
+    from config import MONGO_DB_PASSWORD
+
+    print("""\n\nWARNING: Secrets NOT stored on Git (DB link, api-key, etc.). 
+        Using default (wrong) values from configuration file.
+
+        Please edit the configuration file manually. 
+        If already done, ignore this message.""")
+    return MONGO_DB_PASSWORD
 
 
 def password(env_var: str) -> str:
     val = os.environ.get(env_var)
     if not val:
-        raise ValueError("You haven't set the password environment variable.")
+        return fallback_db_password()
     return val
 
 
