01cdd17411ef0f77a6abe74ff7a531b8498f8fe3
user <kgalsjuwtjuatsnh@asd.edu>
2022-06-10 09:17:29 +0300

    CHG: shorter and more descriptive message when loading secrets from config.
    TODO: ensure MongoDB partial link in GitHub isn't a problem.
    
    TODO: check docs if dependencies.py is where middleware goes. https://fastapi.tiangolo.com/tutorial/bigger-applications/#another-module-with-apirouter

--- secret_handler.py
+++ secret_handler.py
@@ -6,11 +6,9 @@ PASS_ENV_VAR = 'MONGODB_MOVIES_USER0_PASS'
 def fallback_db_password():
     from config import MONGO_DB_PASSWORD
 
-    print("""\n\nWARNING: Secrets NOT stored on Git (DB link, api-key, etc.). 
-        Using default (wrong) values from configuration file.
-
-        Please edit the configuration file manually. 
-        If already done, ignore this message.""")
+    print("""============================
+        Loading secrets from config, instead of env-variables. 
+        Make sure you've edit it. If already done, ignore this message.""")
     return MONGO_DB_PASSWORD
 
 
