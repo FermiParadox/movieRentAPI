fa148422b4dac01ebeadabb1e7f013e0ad130906
user <10809024+FermiParadox@users.noreply.github.com>
2022-06-11 00:59:26 +0300

    DEL: removed redundant code; PyCharm takes care of running the server. CHG: fake email for commits changed to real email (GitHub-noreply email); due to it no commits were attributed to me in GitHub.
    NEXT: edit the email in all commits (see https://stackoverflow.com/questions/750172/how-to-change-the-author-and-committer-name-and-e-mail-of-multiple-commits-in-gi)
    
    TODO: ensure MongoDB partial link in GitHub isn't a problem.
    
    TODO: check docs if dependencies.py is where middleware goes. https://fastapi.tiangolo.com/tutorial/bigger-applications/#another-module-with-
    
    TODO: try Python 3.10 and check Enums to auto

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI
-import uvicorn
 
 from routers import get_all_movies
 from routers import post_movies_cat_x
@@ -8,6 +7,4 @@ app = FastAPI()
 app.include_router(get_all_movies.router)
 app.include_router(post_movies_cat_x.router)
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
-    # Visit http://127.0.0.1:8000/docs# to test the endpoints.
+# Visit http://127.0.0.1:8000/docs# to test the endpoints.
