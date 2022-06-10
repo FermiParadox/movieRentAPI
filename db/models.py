9d78e66785186e97213a990ebbf41bb924d38333
user <kgalsjuwtjuatsnh@asd.edu>
2022-06-10 12:51:27 +0300

    CHG: renamed
    TODO: ensure MongoDB partial link in GitHub isn't a problem.
    
    TODO: check docs if dependencies.py is where middleware goes. https://fastapi.tiangolo.com/tutorial/bigger-applications/#another-module-with-
    
    TODO: try Python 3.10 and check Enums to auto

--- db/models.py
+++ db/models.py
@@ -1,5 +1,4 @@
 from typing import List
-
 from mongoengine import Document, StringField, IntField, DictField, ListField, ValidationError
 
 from db._base import ALL_CATEGORIES
@@ -9,13 +8,13 @@ class User(Document):
     id = IntField(min_value=1, unique=True, required=True)
     name = StringField(max_length=50, required=True)
     # DO NOT store passwords; it's not safe.
-    passphrase = StringField(min_length=12, max_length=70)
+    passphrase_hash = StringField(min_length=12, max_length=70)
     balance = IntField(min_value=0, max_value=1000, default=0)
     rented_movies = DictField()
 
 
 # ---------------------------------------------------------------------------
-def validation_movie_categories(categories: List) -> None:
+def validate_movie_categories(categories: List) -> None:
     disallowed_categories = set(categories) - ALL_CATEGORIES
     if disallowed_categories:
         raise ValidationError(f'The following categories are not allowed: {disallowed_categories}')
@@ -24,4 +23,4 @@ def validation_movie_categories(categories: List) -> None:
 class Movie(Document):
     id_ = IntField(min_value=1, unique=True, required=True)
     title = StringField(max_length=50, required=True)
-    categories = ListField(validation=validation_movie_categories, required=True)
+    categories = ListField(validation=validate_movie_categories, required=True)
