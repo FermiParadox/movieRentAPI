aea30fafc8d3d1ec0813e464f9ad36047f204c10
user <10809024+FermiParadox@users.noreply.github.com>
2022-06-12 12:25:59 +0300

    ADD: tests for endpoints (all tests pass when ran individually)
    BUG: When running all tests, tests of main.py fail with "mongoengine.connection.ConnectionFailure: You have not defined a default connection". Individually running test_main works fine.
    
    TODO: edit the email in all commits, including my other projects (see https://stackoverflow.com/questions/750172/how-to-change-the-author-and-committer-name-and-e-mail-of-multiple-commits-in-gi). Apparently being logged in GitHub through PyCharm doesn't attribute commits to me when using a fake email. Instead, the noreply email of GitHub must be used for anonymity.
    
    TODO: ensure MongoDB partial link in GitHub isn't a problem.
    
    TODO: check docs if dependencies.py is where middleware goes. https://fastapi.tiangolo.com/tutorial/bigger-applications/#another-module-with-
    
    TODO: try Python 3.10 and check Enums to auto

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,26 +1,44 @@
 from unittest import TestCase
 from fastapi.testclient import TestClient
 
-from endpoint_paths import ALL_MOVIES, MOVIES_BY_CAT
 from main import app
 
 
 class TestAllMovies(TestCase):
+    def setUp(self) -> None:
+        from endpoint_paths import ALL_MOVIES
+        self.url = ALL_MOVIES.full
+
     def test_ok_response(self):
-        response = TestClient(app=app).get(ALL_MOVIES.full)
+        response = TestClient(app=app).get(self.url)
         code = response.status_code
         self.assertTrue(response.ok, msg=f'Response code: {code}')
 
+    def test_response_contains_list(self):
+        response = TestClient(app=app).get(self.url)
+        data = response.json()
+        self.assertIsInstance(data, list)
 
-class TestMovies(TestCase):
-    def test_ok_response(self):
-        response = TestClient(app=app).post(url=MOVIES_BY_CAT.full,
+
+class TestMoviesByCategory(TestCase):
+    def setUp(self) -> None:
+        from endpoint_paths import MOVIES_BY_CAT
+        self.url = MOVIES_BY_CAT.full
+
+    def test_response_contains_list(self):
+        response = TestClient(app=app).post(url=self.url,
                                             json={'categories': ['comedy', 'action']})
+        data = response.json()
+        self.assertIsInstance(data, list)
+
+    def test_no_category_responds_422(self):
+        response = TestClient(app=app).post(url=self.url,
+                                            json={})
         code = response.status_code
-        self.assertTrue(response.ok, msg=f'Response code: {code}')
+        self.assertEqual(422, code, msg=f'Response code: {code}')
 
     def test_wrong_category_responds_422(self):
-        response = TestClient(app=app).post(url=MOVIES_BY_CAT.full,
+        response = TestClient(app=app).post(url=self.url,
                                             json={'categories': ['kdf9356a', 'action']})
         code = response.status_code
         self.assertEqual(422, code, msg=f'Response code: {code}')
