7b416a61b62264304e74611c3360108247403cfe
user <10809024+FermiParadox@users.noreply.github.com>
2022-06-12 18:40:00 +0300

    CHG: placed imports on same line
    BUG: Individually running test_main works fine. When running all tests, tests of main.py fail with "mongoengine.connection.ConnectionFailure: You have not defined a default connection".
    
    TODO: edit the email in all commits, including my other projects (see https://stackoverflow.com/questions/750172/how-to-change-the-author-and-committer-name-and-e-mail-of-multiple-commits-in-gi). Apparently being logged in GitHub through PyCharm doesn't attribute commits to me when using a fake email. Instead, the noreply email of GitHub must be used for anonymity.
    
    TODO: ensure MongoDB partial link in GitHub isn't a problem.
    
    TODO: check docs if dependencies.py is where middleware goes. https://fastapi.tiangolo.com/tutorial/bigger-applications/#another-module-with-
    
    TODO: try Python 3.10 and check Enums to auto

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from fastapi import FastAPI
 
 from data.database import connect_to_production_db
-from routers import get_all_movies
-from routers import post_movies_cat_x
+from routers import get_all_movies, post_movie_by_id, post_movies_cat_x
 
 connect_to_production_db()
 
 app = FastAPI()
 app.include_router(get_all_movies.router)
 app.include_router(post_movies_cat_x.router)
+app.include_router(post_movie_by_id.router)
 
 # Visit http://127.0.0.1:8000/docs# to test the endpoints.
