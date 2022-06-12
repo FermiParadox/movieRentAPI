6b221aa413aa9cbadb02502daade9aebdfc39009
user <10809024+FermiParadox@users.noreply.github.com>
2022-06-12 23:12:52 +0300

    CHG: refactored main-tests
    TODO: ensure DB calls are async
    
    BUG: Individually running test_main works fine. When running all tests, tests of main.py fail with "mongoengine.connection.ConnectionFailure: You have not defined a default connection".
    
    TODO: edit the email in all commits, including my other projects (see https://stackoverflow.com/questions/750172/how-to-change-the-author-and-committer-name-and-e-mail-of-multiple-commits-in-gi). Apparently being logged in GitHub through PyCharm doesn't attribute commits to me when using a fake email. Instead, the noreply email of GitHub must be used for anonymity.
    
    TODO: ensure MongoDB partial link in GitHub isn't a problem.
    
    TODO: check docs if dependencies.py is where middleware goes. https://fastapi.tiangolo.com/tutorial/bigger-applications/#another-module-with-
    
    TODO: try Python 3.10 and check Enums to auto

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,9 +3,12 @@ from fastapi.testclient import TestClient
 
 from main import app
 
-print(f"Tests in {__name__} don't fail when run individually."
-      f"They do fail when running all tests in /tests."
+print(f"Tests in {__name__} don't fail when run individually.\n"
+      f"They do fail when running all tests in /tests.\n"
       f"There's probably some bug.")
+
+
+client = TestClient(app=app)
 
 
 class TestAllMovies(TestCase):
@@ -14,12 +17,12 @@ class TestAllMovies(TestCase):
         self.url = ALL_MOVIES.full
 
     def test_ok_response(self):
-        response = TestClient(app=app).get(self.url)
+        response = client.get(self.url)
         code = response.status_code
         self.assertTrue(response.ok, msg=f'Response code: {code}')
 
     def test_response_contains_dict(self):
-        response = TestClient(app=app).get(self.url)
+        response = client.get(self.url)
         data = response.json()
         self.assertIsInstance(data, dict)
 
@@ -30,19 +33,19 @@ class TestMoviesByCategory(TestCase):
         self.url = MOVIES_BY_CAT.full
 
     def test_response_contains_list(self):
-        response = TestClient(app=app).post(url=self.url,
+        response = client.post(url=self.url,
                                             json={'categories': ['comedy', 'action']})
         data = response.json()
         self.assertIsInstance(data, list)
 
     def test_no_category_responds_422(self):
-        response = TestClient(app=app).post(url=self.url,
+        response = client.post(url=self.url,
                                             json={})
         code = response.status_code
         self.assertEqual(422, code, msg=f'Response code: {code}')
 
     def test_wrong_category_responds_422(self):
-        response = TestClient(app=app).post(url=self.url,
+        response = client.post(url=self.url,
                                             json={'categories': ['kdf9356a', 'action']})
         code = response.status_code
         self.assertEqual(422, code, msg=f'Response code: {code}')
@@ -57,22 +60,22 @@ class TestMovieByID(TestCase):
         self.url_for_movie_2 = self.url + '2'
 
     def test_wrong_id_responds_422(self):
-        response = TestClient(app=app).post(url=self.url + '65234234')
+        response = client.post(url=self.url + '65234234')
         code = response.status_code
         self.assertEqual(422, code, msg=f'Response code: {code}')
 
     def test_ok_response(self):
-        response = TestClient(app=app).post(self.url_for_movie_2)
+        response = client.post(self.url_for_movie_2)
         code = response.status_code
         self.assertTrue(response.ok, msg=f'Response code: {code}')
 
     def test_details_id_title(self):
-        response = TestClient(app=app).post(self.url_for_movie_2)
+        response = client.post(self.url_for_movie_2)
         self.assertIn('title', response.json())
         self.assertIn('id_', response.json())
         self.assertIn('categories', response.json())
         self.assertIn('details', response.json())
 
     def test_categories_not_empty(self):
-        response = TestClient(app=app).post(self.url_for_movie_2)
+        response = client.post(self.url_for_movie_2)
         self.assertTrue(response.json()['categories'])
