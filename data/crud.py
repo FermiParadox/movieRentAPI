72101177c8c66bbb0c3b99d78d22d06e4cc195bc
user <10809024+FermiParadox@users.noreply.github.com>
2022-06-12 11:00:14 +0300

    ADD: DB call in movie-list
    BUG: test of main.py fails with "mongoengine.connection.ConnectionFailure: You have not defined a default connection"
    
    TODO: edit the email in all commits, including my other projects (see https://stackoverflow.com/questions/750172/how-to-change-the-author-and-committer-name-and-e-mail-of-multiple-commits-in-gi). Apparently being logged in GitHub through PyCharm doesn't attribute commits to me when using a fake email. Instead, the noreply email of GitHub must be used for anonymity.
    
    TODO: ensure MongoDB partial link in GitHub isn't a problem.
    
    TODO: check docs if dependencies.py is where middleware goes. https://fastapi.tiangolo.com/tutorial/bigger-applications/#another-module-with-
    
    TODO: try Python 3.10 and check Enums to auto

--- data/crud.py
+++ data/crud.py
@@ -1,8 +1,12 @@
+from data.database import connect_to_production_db
+from data.models import Movie
 from data.schema import MovieCategories, MovieIDList
+
+connect_to_production_db()
 
 
 def get_all_movies() -> MovieIDList:
-    return []
+    return [t.title for t in Movie.objects()]
 
 
 def post_movies_of_categories_x(categories: MovieCategories) -> MovieIDList:
