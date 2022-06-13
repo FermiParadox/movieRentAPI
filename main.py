85f5e0fab5cde8548fb45fa5d0542aa8f2f2b561
user <10809024+FermiParadox@users.noreply.github.com>
2022-06-13 23:07:17 +0300

    CHG: rent-movie POST->PUT
    NEXT: add return endpoint
    NEXT: add balance reducer + schedule
    NEXT: add cost class + endpoint
    
    BUG: Individually running test_main works fine. When running all tests, tests of main.py fail with "mongoengine.connection.ConnectionFailure: You have not defined a default connection".
    
    TODO: edit the email in all commits, including my other projects (see https://stackoverflow.com/questions/750172/how-to-change-the-author-and-committer-name-and-e-mail-of-multiple-commits-in-gi). Apparently being logged in GitHub through PyCharm doesn't attribute commits to me when using a fake email. Instead, the noreply email of GitHub must be used for anonymity.
    
    TODO: ensure MongoDB partial link in GitHub isn't a problem.
    
    TODO: check docs if dependencies.py is where middleware goes. https://fastapi.tiangolo.com/tutorial/bigger-applications/#another-module-with-
    
    TODO: try Python 3.10 and check Enums to auto

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 
 from data.database import connect_to_production_db
-from routers import GET_all_movies, GET_movie_by_id, POST_movies_by_cat, POST_rent_movie
+from routers import GET_all_movies, GET_movie_by_id, POST_movies_by_cat, PUT_rent_movie
 
 connect_to_production_db()
 
@@ -9,6 +9,6 @@ app = FastAPI()
 app.include_router(GET_all_movies.router)
 app.include_router(GET_movie_by_id.router)
 app.include_router(POST_movies_by_cat.router)
-app.include_router(POST_rent_movie.router)
+app.include_router(PUT_rent_movie.router)
 
 # Visit http://127.0.0.1:8000/docs# to test the endpoints.
--- routers/PUT_rent_movie.py
+++ routers/PUT_rent_movie.py
@@ -5,7 +5,7 @@ from routers._base import router
 import data.crud
 
 
-@router.post(path=_endpoint_paths.RENT.fastapi_format)
+@router.put(path=_endpoint_paths.RENT.fastapi_format)
 async def post_rent_movie(movie_id: int, req: Request):
     params = req.query_params
     return data.crud.RentedMovieHandler().rent_movie(movie_id=movie_id, user_id=params['user_id'])
