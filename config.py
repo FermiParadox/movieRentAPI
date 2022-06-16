b5168d77c2a072d094c529473e200858aedf124e
user <10809024+FermiParadox@users.noreply.github.com>
2022-06-16 23:48:49 +0300

    CHG: comment now matches constant-name.
    NEXT: clean-up code; refactor god-objects
    
    BUG: Individually running test_main works fine. When running all tests, tests of main.py fail with "mongoengine.connection.ConnectionFailure: You have not defined a default connection".
    
    TODO: edit the email in all commits, including my other projects (see https://stackoverflow.com/questions/750172/how-to-change-the-author-and-committer-name-and-e-mail-of-multiple-commits-in-gi). Apparently being logged in GitHub through PyCharm doesn't attribute commits to me when using a fake email. Instead, the noreply email of GitHub must be used for anonymity.
    
    TODO: ensure MongoDB partial link in GitHub isn't a problem.
    
    TODO: check docs if dependencies.py is where middleware goes. https://fastapi.tiangolo.com/tutorial/bigger-applications/#another-module-with-
    
    TODO: try Python 3.10 and check Enums to auto

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ JWT_SECRET_MESSAGE = 'some_stored_value_server-side'
 JWT_ALGORITHM = "HS256"
 JWT_DURATION_HOURS = 24 * 30
 
-# Create an "IGNORE_GIT_SECRETS.py" file and place MONGO_DB_LINK there
+# Create an "IGNORE_GIT_SECRETS.py" file and place MONGODB_LINK there
 # since that filename is ignored on git commits.
 
 # Or edit this (WARNING: don't accidentally push it on GitHub)
